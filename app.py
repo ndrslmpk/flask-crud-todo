19a2101766e07cf744e7dd5906f5d09f3ea45bf8
ndrslmpk <andreas.lempik@gmail.com>
2022-04-23 15:53:45 +0200

    Todolist#Create operation added

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ def index():
 
 @app.route('/create', methods=['POST'])
 def create():
+  """Creates a todo in the default todo-list (id: 1)"""
+
   error = False
   body = {}
   try:
@@ -68,8 +70,30 @@ def create():
     abort(400)
   return jsonify(body)
 
+  @app.route('lists/<uint:_id>/todo/create', methods=['POST'])
+  def create_list_todo(_id):
+    error = False
+    body = {}
+    try:
+      _description = request.get_json()['description'] # returns a dictionary 
+      _todo = Todo(description = _description)
+      db.session.add(_todo)
+      db.session.commit()
+      body['description'] = _todo.description 
+      body['completed'] = _todo.completed
+    except:
+      error=True
+      db.session.rollback()
+      print(sys.exc_info())
+    finally:
+      db.session.close()
+    if error:
+      abort(400)
+    return jsonify(body)
+
+
 @app.route('/<int:_id>', methods=['POST'])
-def update():
+def update(_id):
   error = False
   body = {}
   try:
@@ -141,7 +165,29 @@ def set_completed_todo(_id):
 @app.route('/lists/<int:list_id>', methods=['GET'])
 def get_list_todos(list_id):
   return render_template('index.html',
-  lists=TodoList.query.all(),
-  active_list=TodoList.query.get(list_id),
-  todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
   )
+
+@app.route('/lists/create', methods=['POST'])
+def create_list():
+  body = {}
+  error = False
+  print(request.get_json())
+  try:
+    _name = request.get_json()['name']
+    _todolist = TodoList(name = _name)
+    db.session.add(_todolist)
+    db.session.commit()
+    body['id'] = _todolist.id
+    body['name'] = _todolist.name 
+  except:
+    error=True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort(400)
+  return jsonify(body)
